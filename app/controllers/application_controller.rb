aaa426fca8ba3a008b2141cff6540cd22b8b7ff3
TH <tharada0854@gmail.com>
2019-01-07 07:12:04 +0000

    Userモデルのstrong_parametersを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_sign_up_params, if: :devise_controller?
+  before_action :configure_account_update_params, if: :devise_controller?
+
+  protected
+
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
