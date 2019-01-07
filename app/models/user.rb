16ceea064fcd04016109c3b161ec9dc5a76ea8c6
TH <tharada0854@gmail.com>
2019-01-07 08:11:29 +0000

    Userモデルにenumとバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  enum role: { common: 0, admin: 1 }
+  devise :database_authenticatable, :registerable, :validatable
+
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :role, presence: true, inclusion: { in: User.roles.keys }
 end
