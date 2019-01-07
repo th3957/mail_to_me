d51fd4577ab0845e150514840efdc63f701e81fa
TH <tharada0854@gmail.com>
2019-01-07 07:09:58 +0000

    Userモデルのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   root to: 'travels#index'
 
-  devise_for :users, skip: :all
+  devise_for :users, controllers: {
+    registrations:'users/registrations'
+  }
 
+  resources :users, only: [:show]
   resources :travels
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
