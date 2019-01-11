c4b64b653258536a24f0a5d0b0e9885ad5bd6cf5
TH <tharada0854@gmail.com>
2019-01-11 04:49:08 +0000

    Noteモデルのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
 
   resource :user, only: [:show]
   resources :travels
+  resources :notes
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
