328e8340db9e87f785f8659a970615f2fa6788dc
TH <tharada0854@gmail.com>
2019-01-13 07:05:21 +0000

    Noteモデルにバリデーションを追加

--- app/models/note.rb
+++ app/models/note.rb
@@ -2,4 +2,7 @@ class Note < ApplicationRecord
   mount_uploader :image, ImagesUploader
 
   belongs_to :travel
+
+  validates :content, presence: true, length: { in: 1..255 }
+  validates :importance, inclusion: { in: [true, false] }
 end
