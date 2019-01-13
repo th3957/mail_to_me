39b5a577be1d900fb3b244a1b2f9a9780739872e
TH <tharada0854@gmail.com>
2019-01-13 07:06:07 +0000

    Cardモデルのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resource :user, only: [:show]
   resources :travels
   resources :notes
+  resources :cards, except: [:index]
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
