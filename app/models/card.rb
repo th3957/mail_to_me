83d0fde454abbf7baac2ffd981b5edb2f6b9e1a6
TH <tharada0854@gmail.com>
2019-01-13 04:47:39 +0000

    Travel,Card,Imageモデルにアソシエーションを設定

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,2 +1,5 @@
 class Card < ApplicationRecord
+  belongs_to :travel
+  has_many :images, inverse_of: :card, dependent: :destroy
+  accepts_nested_attributes_for :images, allow_destroy: true, reject_if: :all_blank
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,5 @@
 class Image < ApplicationRecord
+  mount_uploader :data, ImagesUploader
+
+  belongs_to :card, inverse_of: :images
 end
