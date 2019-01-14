6f2f17f2070afda3b417f71dcbbc9c138469a84d
TH <tharada0854@gmail.com>
2019-01-14 07:28:37 +0000

    Cardsコントローラーを作成

---
+++ app/controllers/cards_controller.rb
@@ -0,0 +1,54 @@
+class CardsController < ApplicationController
+  before_action :set_card, only: [:edit, :show, :update, :destroy]
+  before_action :set_associated_travel, only: [:create, :show, :destroy]
+
+  def new
+    @card = Card.new
+    Image::FORM.times { @card.images.build }
+  end
+
+  def create
+    @card = @associated_travel.cards.build(card_params)
+    if @card.save
+      redirect_to card_path(@card), notice: 'Successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+    (Image::FORM - @card.images.count).times { @card.images.build }
+  end
+
+  def update
+    if @card.update(card_params)
+      redirect_to card_path(@card), notice: 'Successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @card.destroy
+    redirect_to travel_path(@associated_travel), notice: 'Successfully deleted.'
+  end
+
+  private
+
+  def set_card
+    @card = Card.find(params[:id])
+  end
+
+  def card_params
+    params.require(:card).permit(:title,
+                                 :sentence,
+                                 :frame_style,
+                                 :font_style,
+                                 :travel_id,
+                                 images_attributes:[:id, :data]
+                                 )
+  end
+end
