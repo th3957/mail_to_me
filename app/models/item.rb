1a24c4697f3ee2b5154a4a62d80eb38fa78ae606
TH <tharada0854@gmail.com>
2019-01-18 09:47:01 +0000

    List,Itemモデルにバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,5 +3,7 @@ class Item < ApplicationRecord
 
   belongs_to :list, inverse_of: :items
 
+  validates :name, presence: true, length: { in: 1..20 }
+  validates :remark, length: { in: 1..50 }
   validates_presence_of :list
 end
--- app/models/list.rb
+++ app/models/list.rb
@@ -3,5 +3,6 @@ class List < ApplicationRecord
   has_many :items, inverse_of: :list, dependent: :destroy
   accepts_nested_attributes_for :items, allow_destroy: true, reject_if: :all_blank
 
+  validates :title, presence: true, length: { in: 1..20 }
   validates_presence_of :travel
 end
