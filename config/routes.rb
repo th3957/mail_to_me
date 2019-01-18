f4cf4d9e948a1e31b5e3e434ace396615406a5a9
TH <tharada0854@gmail.com>
2019-01-18 09:38:46 +0000

    Listモデルのルーティングを追加

--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,15 @@ Rails.application.routes.draw do
     sessions:      'users/sessions'
   }
 
-  resource :user, only: [:show]
-  resource :user do
+  resource :user, only: [:show] do
     get 'top', on: :collection
   end
   resources :travels
   resources :notes, except: [:new]
   resources :cards, except: [:index]
+  resources :lists, except: [:new] do
+    get 'personal', on: :collection
+  end
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
