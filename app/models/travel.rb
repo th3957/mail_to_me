8544de25cfea5929c96b375fe5193adae379b39b
TH <tharada0854@gmail.com>
2019-01-19 06:26:58 +0000

    Travelモデルにバリデーションを追加

--- app/models/travel.rb
+++ app/models/travel.rb
@@ -6,13 +6,16 @@ class Travel < ApplicationRecord
   has_many :cards, dependent: :destroy
   has_many :notes, dependent: :destroy
   has_many :areas, inverse_of: :travel, dependent: :destroy
-  accepts_nested_attributes_for :areas, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :areas,
+                                allow_destroy: true,
+                                reject_if: :all_blank,
+                                limit: 10
 
   validates :title, presence: true, length: { in: 1..20 }
   validates :departured_at, presence: true
   validates :returend_at, presence: true
   validate :return_before_departure_date
-  validates :travel_image, presence: true, image: true, file_size: { less_than: 5.megabytes }
+  validates :travel_image, image: true, file_size: { less_than: 5.megabytes }
   validates_presence_of :user
 
   def return_before_departure_date
