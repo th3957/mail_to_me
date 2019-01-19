1b5a771eeecf8cf9c4769d7585a83195fae1a8f9
TH <tharada0854@gmail.com>
2019-01-19 09:34:51 +0000

    Listモデルにバリデーションを追加

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,7 +1,10 @@
 class List < ApplicationRecord
   belongs_to :travel
   has_many :items, inverse_of: :list, dependent: :destroy
-  accepts_nested_attributes_for :items, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :items,
+                                allow_destroy: true,
+                                reject_if: :all_blank,
+                                limit: 100
 
   validates :title, presence: true, length: { in: 1..20 }
   validates_presence_of :travel
