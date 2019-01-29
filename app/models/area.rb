98726e7dfa87061f61a6c0755f70a26b45f6798d
TH <tharada0854@gmail.com>
2019-01-29 07:59:05 +0000

    Travel,Areaモデルのバリデーションを修正

--- app/models/area.rb
+++ app/models/area.rb
@@ -3,5 +3,14 @@ class Area < ApplicationRecord
 
   validates :country, presence: true, length: { in: 1..30 }
   validates :place, presence: true, length: { in: 1..30 }
+  validates :arrived_at, presence: true
+  validates :left_at, presence: true
+  validate :leave_before_arrived_date?
   validates_presence_of :travel
+
+  def leave_before_arrived_date?
+    if left_at < arrived_at
+      errors.add(:left_at, ": Can't set leave date before arrived date.")
+    end
+  end
 end
--- app/models/travel.rb
+++ app/models/travel.rb
@@ -12,15 +12,15 @@ class Travel < ApplicationRecord
                                 limit: 10
 
   validates :title, presence: true, length: { in: 1..20 }
-  validates :departured_at, presence: true
-  validates :returend_at, presence: true
-  validate :return_before_departure_date
+  validates :departed_at, presence: true
+  validates :returned_at, presence: true
+  validate :return_before_departed_date?
   validates :travel_image, image: true, file_size: { less_than: 5.megabytes }
   validates_presence_of :user
 
-  def return_before_departure_date
-    if returend_at < departured_at
-      errors.add(:returend_at, ": Can't set return date before departure date.")
+  def return_before_departed_date?
+    if returned_at < departed_at
+      errors.add(:returned_at, ": Can't set return date before departed date.")
     end
   end
 end
