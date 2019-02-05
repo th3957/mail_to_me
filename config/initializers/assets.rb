daaf351dfbf43097ea027cc7de8c25a9a0beb9d6
TH <tharada0854@gmail.com>
2019-02-05 07:33:35 +0000

    devise関係のページを修正

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,6 +12,8 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 Rails.application.config.assets.precompile += %w( landing.css
+                                                  devise.css
+                                                  registrations_edit.css
                                                   sidebar.css
                                                   users.css
                                                   travels.css
