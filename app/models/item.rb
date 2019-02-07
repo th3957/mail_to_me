90e44c9aa13c42ebe39a26c3c9ec55cc010b6a11
TH <tharada0854@gmail.com>
2019-02-07 06:08:22 +0000

    全体的にビューを調整

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,6 @@ class Item < ApplicationRecord
   belongs_to :list, inverse_of: :items
 
   validates :name, presence: true, length: { in: 1..20 }
-  validates :remark, length: { in: 1..50 }
+  validates :remark, length: { maximum: 50 }
   validates_presence_of :list
 end
