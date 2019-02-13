0003cf77b467caee85ab509d30c6026f0730de8f
TH <tharada0854@gmail.com>
2019-02-13 06:40:55 +0000

    Cardモデルのenumを修正

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,10 +1,19 @@
 class Card < ApplicationRecord
   belongs_to :travel
   has_many :images, inverse_of: :card, dependent: :destroy
-  accepts_nested_attributes_for :images, allow_destroy: true, reject_if: :all_blank
+  accepts_nested_attributes_for :images,
+                                allow_destroy: true,
+                                reject_if: :all_blank,
+                                limit: 8
 
-  enum frame_style: { frame_sample0: 0, frame_sample1: 1, frame_sample2: 2 }
-  enum font_style: { font_sample0: 0, font_sample1: 1, font_sample2: 2 }
+  enum frame_style: { basic:  0,
+                      tile:   1,
+                     }
+
+  enum font_style: { ipaex_gothic:     0,
+                     yasashisa_gothic: 1,
+                     playball:         2,
+                    }
 
   validates :title, presence: true, length: { in: 1..20 }
   validates :sentence, presence: true, length: { in: 1..255 }
