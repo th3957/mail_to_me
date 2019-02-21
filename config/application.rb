d7c75c4186534b3a3ed91ffe8dcee4b42b1bbfde
TH <tharada0854@gmail.com>
2019-02-21 16:02:23 +0000

    国際化ファイルをビューに反映

--- config/application.rb
+++ config/application.rb
@@ -48,7 +48,9 @@ module MailToMe
 
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    I18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
   end
 end
