f36c344f89d33967924f16244904c2c2b13427f6
TH <tharada0854@gmail.com>
2019-03-14 06:08:47 +0000

    ファビコンを設定

--- config/application.rb
+++ config/application.rb
@@ -50,7 +50,7 @@ module MailToMe
     config.active_record.default_timezone = :local
 
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-    I18n.available_locales = [:en, :ja]
+    config.i18n.available_locales = [:en, :ja]
     config.i18n.default_locale = :ja
   end
 end
